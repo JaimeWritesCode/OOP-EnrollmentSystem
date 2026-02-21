ce7e47d5b2400aacae3e913e556e8ac3b3303ba8
Jaime Lorenzo S. Palacio <itsjustmejaime77@gmail.com>
2026-02-21 16:12:52 +0800

    Added Interactive Scanner (Name and Age)

--- src/main/java/org/example/service/StudentRegistration.java
+++ src/main/java/org/example/service/StudentRegistration.java
@@ -1,9 +1,11 @@
 package org.example.service;
 import org.example.model.Student;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class StudentRegistration {
     private ArrayList<Student> students = new ArrayList();
+    Scanner input = new Scanner(System.in);
 
     //Create
     public void addStudent(Student student){
@@ -19,7 +21,13 @@ public class StudentRegistration {
   public void updateStudent(Student student) {
       for (int i = 0; i < students.size(); i++) {
           if (students.get(i).getstudentID().equals(student.getstudentID())) {
-              students.set(i, student);
+              System.out.print("Enter student name: ");
+              String name = input.nextLine();
+
+              System.out.print("Enter student age: ");
+              String age = input.nextLine();
+
+              students.set(i, new Student(student.getstudentID(), name, age));
               break;
           }
       }
