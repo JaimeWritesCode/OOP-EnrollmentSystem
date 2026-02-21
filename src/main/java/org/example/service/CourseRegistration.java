36f306b534f7a3cf840816dc6dfa9186d7aa2945
Jaime Lorenzo S. Palacio <itsjustmejaime77@gmail.com>
2026-02-21 12:44:56 +0800

    Added: Course registration beginnings

--- src/main/java/org/example/service/CourseRegistration.java
+++ src/main/java/org/example/service/CourseRegistration.java
@@ -1,4 +1,12 @@
 package org.example.service;
+import org.example.model.Course;
+import java.util.ArrayList;
 
 public class CourseRegistration {
+    private ArrayList<Course> courses = new ArrayList();
+
+    //Create
+    public void addCourse(Course course){
+        courses.add(course);
+    }
 }
